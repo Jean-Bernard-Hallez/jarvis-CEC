fdf59acbef5797ef408b1c02e5ca4f5432762a7e
Hallez <jbhallez@aol.com>
2017-02-21 14:42:36 +0100

    Ce plugin Jarvis avec le HDMI CEC sert à Allumer ou Eteindre son TVC. le CEC est un protocole permetant de contrôle un périphérique à l'aide
    You: Allume la télé.
    Jarvis: la télé est alumée...
    
    You: Eteint la télé.
    Jarvis: la télé est éteinte...
    
    You: Etat de la télé ?
    Jarvis: Je vais mesurer son état, patientez...
    Jarvis: La Télévision est arrêtée.

--- fr/functions.sh
+++ fr/functions.sh
@@ -2,11 +2,27 @@ jv_pg_ct_fonction_CEC_ETAT() {
 etatTV=`echo pow 0 | cec-client -s -d 1`
 say "Je vais mesurer son état, patientez..."
 if [[ "$etatTV" =~ "on" ]]; then
-say "La Télvision est en alumée"
+say "La Télévision est alumée"
 fi
 
 if [[ "$etatTV" =~ "standby" ]]; then
-say "La Télvision est arrêtée"
+say "La Télévision est arrêtée"
 fi
-
 } 
+
+if [[ "$etatTV" =~ "unknown" ]]; then
+say "Je n'arrive pas à communiquer avec la télé, elle doit être arrétée par l'interupteur. "
+fi
+} 
+
+
+jv_pg_ct_fonction_CEC_ETEINT() {
+cec-client -s -d 1 >/dev/null 2>&1 
+say "la télé est éteinte"
+}
+
+jv_pg_ct_fonction_CEC_ALLUME() {
+say "j'allume la télé"
+echo on 0 | cec-client -s -d 1 >/dev/null 2>&1 
+}
+
